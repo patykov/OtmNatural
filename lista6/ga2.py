6dddcef7de60ce288b29c3219299a0a43749435f
patykov <patriciakovaleski@gmail.com>
2018-05-20 23:38:22 -0300

    Adding lista6 and some old tests

--- lista6/ga2.py
+++ lista6/ga2.py
@@ -5,21 +5,36 @@ import itertools
 
 
 def J(x):
-	return sum(x)*1.0
+	return sum(x)
 
 
 def GA(tam_populacao, max_geracoes, pc, pm, L):
 	num_filhos = tam_populacao
 	results = [] #[geracao, best, mean, worst]
 
-	P = [np.random.randint(0, 2, size=L) for i in range(tam_populacao)]
+	P = [list(np.random.randint(0, 2, size=L)) for i in range(tam_populacao)]
 	geracao = 0
 
 	while(geracao < max_geracoes):
 		geracao += 1
 
+		# Busca local com profundidade 1
+		aptidao = []
+		for i, x in enumerate(P):
+			best = J(x)
+			for p in range(L):
+				#BitFlip
+				vizinho_x = x[:]
+				vizinho_x[p] = 1 - vizinho_x[p]
+				new_j = J(vizinho_x)
+				if (new_j > best):
+					best = new_j
+					P[i] = vizinho_x
+					# Greedy
+					break 
+			aptidao.append(best)
+
 		# Selecao dos pais (SUS)
-		aptidao = [J(i) for i in P]
 		apt_prob = [a*1.0/sum(aptidao) for a in aptidao]
 		apt_sum = [sum(apt_prob[:i+1]) for i in range(len(apt_prob))]
 
@@ -27,7 +42,7 @@ def GA(tam_populacao, max_geracoes, pc, pm, L):
 		r = np.random.rand()*1.0/tam_populacao
 		for p in range(tam_populacao):
 			while (r <= apt_sum[p]):
-				pais.append(list(P[p]))
+				pais.append(P[p])
 				r = r + (1.0/tam_populacao)
 
 		# Recombinacao
@@ -55,7 +70,7 @@ def GA(tam_populacao, max_geracoes, pc, pm, L):
 		# Selecao de sobreviventes (geracional)
 		P = filhos
 
-		# Resultados para a geracao
+		# Resultados para a geracao (para o grafico)
 		aptidao = [J(i) for i in P]
 		best = max(aptidao)
 		worst = min(aptidao)
--- lista6/lista6-q2b.py
+++ lista6/lista6-q2b.py
@@ -4,15 +4,14 @@ import numpy as np
 
 L = 25
 tam_populacao = 100
-max_geracoes = 50000
+max_geracoes = 100
 pc = 0.7  #prob de recombinacao
 pm = 1.0/L #prob de mutacao
 
 
 geracoes = []
-for i in range(10):
+for i in range(100):
 	results = q2.GA(tam_populacao, max_geracoes, pc, pm, L)
-	print(i, results[-1, 0])
 	geracoes.append(results[-1, 0])
 
 
